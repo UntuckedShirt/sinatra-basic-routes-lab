1cf73406f605e23a08b4ea57d78ff7b6baeeecb3
Nathan Miller <natem31190@gmail.com>
2021-03-01 13:47:56 -0500

    commit tree 2180fb1a793d0ec70a6ee3b9c724965764e69911

--- app.rb
+++ app.rb
@@ -1,4 +1,49 @@
-require_relative 'config/environment'
+# require_relative 'config/environment'
 
+# class App < Sinatra::Base
+
+# get '/' do
+#     "Hello, World!"
+# end
+
+
+# get '/name' do
+#     @name = name.all
+
+#     erb :'name/index.html.erb'
+#     "My name is __"
+# end
+
+# get '/hometown' do
+#     @hometown = hometown.all
+
+#     erb :'hometown/index.html.erb'
+#     "My hometown is __"
+# end
+
+# get '/favorite-song' do
+#     @favorite-song = favorite-song.all
+
+#     erb :'favorite-song/index.html.erb'
+#     "My favorite song is __"
+
+# end
+# end
+
+
+
+require_relative 'config/environment'
 class App < Sinatra::Base
+get '/' do
+        "Hello, World!"
+end
+get '/name' do
+    "My name is Nate"
+end
+get '/hometown' do
+    "My hometown is Some City"
+end
+get '/favorite-song' do
+    "My favorite song is some song"
+end
 end
